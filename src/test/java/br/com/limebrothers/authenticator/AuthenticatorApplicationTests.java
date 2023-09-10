21370f6d06fe6813c0079718a67f4042dab0149d
Leonardo Coldebella <63103712+limebrothers@users.noreply.github.com>
2023-09-10 12:06:37 -0300

    Initial structure

---
+++ src/test/java/br/com/limebrothers/authenticator/AuthenticatorApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.limebrothers.authenticator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
